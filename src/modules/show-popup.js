5dfcee5121e76ce342ab0c159d0d432d422d7cc1
Andrei <i.andrei.abramov@gmail.com>
2021-08-01 18:07:58 +0300

    Доработана функция открытия попапа

--- src/modules/show-popup.js
+++ src/modules/show-popup.js
@@ -1,7 +1,6 @@
 import {getFilmPopupHtml} from '../view/film-popup';
-import {insertHtmlElement} from '../services/utils';
+import {insertHtmlElement, isEscEvent} from '../services/utils';
 import {moviesData} from '../mock-data/film-data';
-import {isEscEvent} from '../services/utils';
 
 export const setOpenPopupHandler = () => {
   const cardsContainer = document.querySelector('.films-list__container');
@@ -10,22 +9,28 @@ export const setOpenPopupHandler = () => {
   //todo: Реализовать подстановку данных того фильма, на который кликнули
   const filmPopup = getFilmPopupHtml(moviesData[0]);
 
-  cardsContainer.addEventListener('click', (evt) => {
-    insertHtmlElement(popupContainer, filmPopup, 'afterend');
-    const popup = document.querySelector('.film-details');
-    const closeButton = popup.querySelector('.film-details__close-btn');
-    const closePopupByEsc = (evt) => {
-      if (isEscEvent(evt)) {
+  cardsContainer.addEventListener('click', (clickEvt) => {
+    if (
+      clickEvt.target.classList.contains('film-card__poster') ||
+      clickEvt.target.classList.contains('film-card__title') ||
+      clickEvt.target.classList.contains('film-card__comments')
+    ) {
+      insertHtmlElement(popupContainer, filmPopup, 'afterend');
+      const popup = document.querySelector('.film-details');
+      const closeButton = popup.querySelector('.film-details__close-btn');
+      const closePopupByEsc = (keyDownEvt) => {
+        if (isEscEvent(keyDownEvt)) {
+          popup.remove();
+          document.removeEventListener('keydown', closePopupByEsc);
+        }
+      };
+      const closePopupByClick = () => {
         popup.remove();
         document.removeEventListener('keydown', closePopupByEsc);
-      }
-    };
-    const closePopupByClick = () => {
-      popup.remove();
-      document.removeEventListener('keydown', closePopupByEsc);
-    };
+      };
 
-    document.addEventListener('keydown', closePopupByEsc);
-    closeButton.addEventListener('click', closePopupByClick);
+      document.addEventListener('keydown', closePopupByEsc);
+      closeButton.addEventListener('click', closePopupByClick);
+    }
   });
 };
