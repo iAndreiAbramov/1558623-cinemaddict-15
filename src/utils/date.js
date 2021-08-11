1f5c403063a6b7f1d91927a24f0aac37bc906425
Andrei <i.andrei.abramov@gmail.com>
2021-08-11 13:32:27 +0300

    Борьба с магическими цифрами

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,9 +1,12 @@
 import dayjs from 'dayjs';
 import {getRandomInteger} from './common';
 
+const DATE_INTERVAL_IN_DAYS = 365;
+const MINUTES_IN_HOUR = 60;
+
 // Возвращает случайную дату в интервале 365 дней назад с текущей даты по текущую дату в указанном формате.
 export const getRandomDate = () => {
-  const day = dayjs().date((getRandomInteger(-365, dayjs().date())));
+  const day = dayjs().date((getRandomInteger(-DATE_INTERVAL_IN_DAYS, dayjs().date())));
   return dayjs(day);
 };
 
@@ -14,7 +17,7 @@ export const reformatDateForComments = (date) => dayjs(date).format('YYYY/MM/DD 
 
 //Возвращает случайную длительность от 60 до 240 минут. Преобразует в формат 3h 24m
 export const transformDuration = (durationInMinutes) => {
-  const minutes = durationInMinutes % 60;
-  const hours = (durationInMinutes - minutes) / 60;
+  const minutes = durationInMinutes % MINUTES_IN_HOUR;
+  const hours = (durationInMinutes - minutes) / MINUTES_IN_HOUR;
   return `${hours}h ${minutes}m`;
 };
