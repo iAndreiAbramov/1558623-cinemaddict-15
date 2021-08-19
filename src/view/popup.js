42063e749e9a533ed76fade692ab4b236b8e8919
Andrei <i.andrei.abramov@gmail.com>
2021-08-20 01:03:24 +0300

    Создан отдельный компонент для нового комментария в попапе. Обновлена отрисовка попапа.

--- src/view/popup.js
+++ src/view/popup.js
@@ -1,7 +1,7 @@
 import {reformatDate} from '../utils/date';
 import AbstractView from './abstract-view';
 
-const getFilmPopupHtml = (filmData) => {
+const getPopupHtml = (filmData) => {
   const {id, comments, filmInfo} = filmData;
   const genres = filmInfo.genre.split(' ').length > 1 ? 'Gengres' : 'Genre';
   return `
@@ -64,41 +64,10 @@ const getFilmPopupHtml = (filmData) => {
           </div>
         </div>
       </div>
-
       <div class="film-details__bottom-container">
         <section class="film-details__comments-wrap">
           <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
           <ul class="film-details__comments-list"></ul>
-
-          <div class="film-details__new-comment">
-            <div class="film-details__add-emoji-label"></div>
-
-            <label class="film-details__comment-label">
-              <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
-            </label>
-
-            <div class="film-details__emoji-list">
-              <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile">
-              <label class="film-details__emoji-label" for="emoji-smile">
-                <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
-              </label>
-
-              <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-sleeping" value="sleeping">
-              <label class="film-details__emoji-label" for="emoji-sleeping">
-                <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
-              </label>
-
-              <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
-              <label class="film-details__emoji-label" for="emoji-puke">
-                <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
-              </label>
-
-              <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
-              <label class="film-details__emoji-label" for="emoji-angry">
-                <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
-              </label>
-            </div>
-          </div>
         </section>
       </div>
     </form>
@@ -106,13 +75,13 @@ const getFilmPopupHtml = (filmData) => {
   `;
 };
 
-export default class FilmPopup extends AbstractView {
+export default class Popup extends AbstractView {
   constructor(filmData) {
     super();
     this._filmData = filmData;
   }
 
   getTemplate() {
-    return getFilmPopupHtml(this._filmData);
+    return getPopupHtml(this._filmData);
   }
 }
